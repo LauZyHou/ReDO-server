1efbba8858b4ad8258fb81013d9534e10cc6c89f
Wu Wenhan <1020645987@qq.com>
2020-10-26 18:38:23 +0800

    Stable

---
+++ src/main/java/com/ecnu/redo/config/ACAO.java
@@ -0,0 +1,36 @@
+package com.ecnu.redo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class ACAO {
+
+    public <T> T myFunc(Class<T> a){
+        try{
+            return a.newInstance();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+       return null;
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");//允许任何域名
+        config.setAllowCredentials(true);//允许Cookie
+        config.addAllowedMethod("*");//允许任何方法
+        config.addAllowedHeader("*");//允许任何头
+        config.setMaxAge(1800l);//设置预检请求保持时间，避免频繁发送预检请求
+
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+        configSource.registerCorsConfiguration("/**", config);
+//        ApplicationContext ctx=new AnnotationConfigApplicationContext();
+//       ACAO a= ctx.getBean(ACAO.class);
+        return new CorsFilter(configSource);
+    }
+}
